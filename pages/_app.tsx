d648dea2b31bf79f8454417a8ff0c2d505ade04c
femiorok <68767356+femiorok@users.noreply.github.com>
2022-09-08 10:54:54 -0700

    initial commit of preliminary features

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
+import React from 'react'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [queryClient] = React.useState(() => new QueryClient())
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>
+  )
 }
 
 export default MyApp
---
+++ pages/components/Hero.js
@@ -0,0 +1,38 @@
+import Image from "next/image"
+import artist_hero from '../../public/artist_hero.png'
+
+const Hero = () => {
+  return (
+    <div className="landingcontainer h-screen w-screen bg-cover bg-bottom flex flex-col items-center justify-center "> 
+      <h1 className="font-extrabold text-white text-5xl text-center sm:text-5xl md:text-5xl lg:text-7xl md:leading-tight">
+        Afrobeats <span className=" bg-clip-text text-transparent bg-gradient-to-tl from-red-800 via-yellow-600 to-yellow-500" >Encyclopedia</span>
+      </h1>
+      <h1 className="font-semibold text-2xl text-center md:text-3xl lg:text-4xl text-white bg-slate-900/20 px-4 mt-4">
+        The perfect place to explore the world's most buzzing genre.
+      </h1>
+      <div className="mt-16 flex flex-col md:flex-row gap-4"> 
+      <a href="/top-200-ng" className="relative p-0.5 inline-flex items-center justify-center font-bold overflow-hidden group rounded-md">
+      <span className="w-full h-full bg-gradient-to-br from-red-800 via-yellow-600 to-yellow-500 group-hover:from-red-800 group-hover:via-yellow-600 group-hover:to-yellow-500 absolute"></span>
+      <span className="relative px-6 py-3 transition-all ease-out bg-gray-900 rounded-md group-hover:bg-opacity-0 duration-400">
+      <span className="relative text-white">Top 200 Songs Nigeria</span>
+      </span>
+      </a>
+      <a href="#_" className="relative p-0.5 inline-flex items-center justify-center font-bold overflow-hidden group rounded-md">
+      <span className="w-full h-full bg-gradient-to-br from-red-800 via-yellow-600 to-yellow-500 group-hover:from-red-800 group-hover:via-yellow-600 group-hover:to-yellow-500 absolute"></span>
+      <span className="relative px-6 py-3 transition-all ease-out bg-gray-900 rounded-md group-hover:bg-opacity-0 duration-400">
+      <span className="relative text-white">Explore Spotify Playlists</span>
+      </span>
+      </a>
+      <a href="#_" className="relative p-0.5 inline-flex items-center justify-center font-bold overflow-hidden group rounded-md">
+      <span className="w-full h-full bg-gradient-to-br from-red-800 via-yellow-600 to-yellow-500 group-hover:from-red-800 group-hover:via-yellow-600 group-hover:to-yellow-500 absolute"></span>
+      <span className="relative px-6 py-3 transition-all ease-out bg-gray-900 rounded-md group-hover:bg-opacity-0 duration-400">
+      <span className="relative text-white">Get Recommendations</span>
+      </span>
+      </a>
+      </div>
+
+    </div>
+  )
+}
+
+export default Hero
